c4203c6b682fe1e100bce925fc408d8379f69ef0
Subani Charis <kharis.subani@gmail.com>
2025-05-09 09:56:47 +0700

    add missfile

--- server.js
+++ server.js
@@ -73,3 +73,7 @@ app.post('/generate-plan', async (req, res) => {
         res.status(500).json({ error: e.message });
     }
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+    console.log(`Server running at http://localhost:${PORT}`));
